c0d6579e46b1fdb0e01d5badba8b9c8753208b7c
Godfrey Best <godfrey.c.best@gmail.com>
2020-07-14 15:55:56 -0400

    add back works

--- src/components/works.js
+++ src/components/works.js
@@ -1,110 +1,94 @@
 import React from 'react'
 
 const Works = () => (
-    {/*
-     <!-- START WORK DESIGN AREA -->
-    <!-- <section id="work" className="work section-padding">
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-12">
-                    <div className="section-title">
-                        <h2>Take a look at my recent works</h2>
-                    </div>
+    <section id="work" classname="work section-padding">
+        <div classname="container">
+          <div classname="row">
+            <div classname="col-sm-12">
+              <div classname="section-title">
+                <h2>Take a look at my recent works</h2>
+              </div>
+            </div>
+          </div>
+          <div classname="row">
+            <ul classname="work">
+              <li classname="filter active" data-filter="all">all</li>
+              <li classname="filter" data-filter=".webdesign">web design</li>
+              <li classname="filter" data-filter=".development">development</li>
+            </ul>
+          </div>
+          <div classname="work-inner">
+            <div classname="row work-posts">
+              <div classname="col-md-4 col-sm-6 mix graphic works">
+                <div classname="work-title">
+                  <h4>Find-A-Spot <span>Web Development</span></h4>
+                  <a href="https://findaspot.herokuapp.com/" classname="work-link" target="_blank" rel="noreferrer">Go To Site</a>
                 </div>
-            </div>
-            <div className="row">
-                <ul className="work">
-                    <li className="filter active" data-filter="all">all</li>
-                    <li className="filter" data-filter=".webdesign">web design</li>
-                    <li className="filter" data-filter=".development">development</li>
-                </ul>
-            </div>
-            <div className="work-inner">
-                <div className="row work-posts">
-                    <!- - START SINGLE WORK DESIGN AREA - ->
-                    <div className="col-md-4 col-sm-6 mix graphic works">
-                       <div className="work-title">
-                               <h4>Find-A-Spot <span>Web Development</span></h4>
-                               <a href="https://findaspot.herokuapp.com/" className="work-link" target="_blank">Go To Site</a>
-                           </div>
-                        <div className="item">
-                            <a href="images/portfolio/work-4.jpg" className="image-popup">
-                                <img src="images/portfolio/work-4.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <!- - END SINGLE WORK DESIGN AREA - ->
-                    <!- - START SINGLE WORK DESIGN AREA - ->
-                    <div className="col-md-4 col-sm-6 mix webdesign works">
-                       <div className="work-title">
-                               <h4>LitterBot <span>Web Development</span></h4>
-                               <a href="https://litterbot.herokuapp.com/" className="work-link" target="_blank">Go to Site</a>
-                           </div>
-                        <div className="item">
-                            <a href="images/portfolio/work-1.jpg" className="image-popup">
-                                <img src="images/portfolio/work-1.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <!- - END SINGLE WORK DESIGN AREA - ->
-                    <!- - START SINGLE WORK DESIGN AREA - ->
-                    <div className="col-md-4 col-sm-6 mix development works">
-                       <div className="work-title">
-                               <h4>Morning Message <span>Web Development</span></h4>
-                               <a href="http://morning-message.herokuapp.com/" className="work-link" target="_blank">Go to Site</a>
-                           </div>
-                        <div className="item">
-                            <a href="images/portfolio/work-5.jpg" className="image-popup">
-                                <img src="images/portfolio/work-5.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <!- - END SINGLE WORK DESIGN AREA - ->
-                    <!- - START SINGLE WORK DESIGN AREA - ->
-                    <div className="col-md-4 col-sm-6 mix webdesign development works">
-                       <div className="work-title">
-                               <h4>SnapSnax <span>Web Development</span></h4>
-                               <a href="https://snap-snax.herokuapp.com/" className="work-link" target="_blank">Go to Site</a>
-                           </div>
-                        <div className="item">
-                            <a href="images/portfolio/work-2.jpg" className="image-popup">
-                                <img src="images/portfolio/work-2.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <!- - END SINGLE WORK DESIGN AREA - ->
-                    <!- - START SINGLE WORK DESIGN AREA - ->
-                    <div className="col-md-4 col-sm-6 mix webdesign works">
-                       <div className="work-title">
-                               <h4>Icebox <span>Web Design</span></h4>
-                               <a href="https://iceboxent.herokuapp.com/" className="work-link" target="_blank">Go to Site</a>
-                           </div>
-                        <div className="item">
-                            <a href="images/portfolio/work-3.jpg" className="image-popup">
-                                <img src="images/portfolio/work-3.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <!- - END SINGLE WORK DESIGN AREA - ->
-                    <!- - START SINGLE WORK DESIGN AREA - ->
-                    <div className="col-md-4 col-sm-6 mix development works">
-                       <div className="work-title">
-                               <h4>Fave Haunts <span>Web / Development</span></h4>
-                               <a href="http://www.favehaunts.com" className="work-link" target="_blank">Go to Site</a>
-                           </div>
-                        <div className="item">
-                            <a href="images/portfolio/favehaunts.jpg" className="image-popup">
-                                <img src="images/portfolio/favehaunts.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <!- - END SINGLE WORK DESIGN AREA - ->
+                <div classname="item">
+                  <a href="images/portfolio/work-4.jpg" classname="image-popup">
+                    <img src="images/portfolio/work-4.jpg" alt="" />
+                  </a>
                 </div>
+              </div>
+              <div classname="col-md-4 col-sm-6 mix webdesign works">
+                <div classname="work-title">
+                  <h4>LitterBot <span>Web Development</span></h4>
+                  <a href="https://litterbot.herokuapp.com/" classname="work-link" target="_blank" rel="noreferrer">Go to Site</a>
+                </div>
+                <div classname="item">
+                  <a href="images/portfolio/work-1.jpg" classname="image-popup">
+                    <img src="images/portfolio/work-1.jpg" alt="" />
+                  </a>
+                </div>
+              </div>
+              <div classname="col-md-4 col-sm-6 mix development works">
+                <div classname="work-title">
+                  <h4>Morning Message <span>Web Development</span></h4>
+                  <a href="http://morning-message.herokuapp.com/" classname="work-link" target="_blank" rel="noreferrer">Go to Site</a>
+                </div>
+                <div classname="item">
+                  <a href="images/portfolio/work-5.jpg" classname="image-popup">
+                    <img src="images/portfolio/work-5.jpg" alt="" />
+                  </a>
+                </div>
+              </div>
+              <div classname="col-md-4 col-sm-6 mix webdesign development works">
+                <div classname="work-title">
+                  <h4>SnapSnax <span>Web Development</span></h4>
+                  <a href="https://snap-snax.herokuapp.com/" classname="work-link" target="_blank" rel="noreferrer">Go to Site</a>
+                </div>
+                <div classname="item">
+                  <a href="images/portfolio/work-2.jpg" classname="image-popup">
+                    <img src="images/portfolio/work-2.jpg" alt="" />
+                  </a>
+                </div>
+              </div>
+              <div classname="col-md-4 col-sm-6 mix webdesign works">
+                <div classname="work-title">
+                  <h4>Icebox <span>Web Design</span></h4>
+                  <a href="https://iceboxent.herokuapp.com/" classname="work-link" target="_blank" rel="noreferrer">Go to Site</a>
+                </div>
+                <div classname="item">
+                  <a href="images/portfolio/work-3.jpg" classname="image-popup">
+                    <img src="images/portfolio/work-3.jpg" alt="" />
+                  </a>
+                </div>
+              </div>
+              <div classname="col-md-4 col-sm-6 mix development works">
+                <div classname="work-title">
+                  <h4>Fave Haunts <span>Web / Development</span></h4>
+                  <a href="http://www.favehaunts.com" classname="work-link" target="_blank" rel="noreferrer">Go to Site</a>
+                </div>
+                <div classname="item">
+                  <a href="images/portfolio/favehaunts.jpg" classname="image-popup">
+                    <img src="images/portfolio/favehaunts.jpg" alt="" />
+                  </a>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </section> -->
-    <!-- / END START WORK DESIGN AREA --></div>
-    */}
+      </section>
 )
 
-export default Works
+export default Works;
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Services from "../components/services"
 import Contact from '../components/contact';
 import Main from '../components/main';
 import Layout from '../components/layout';
-// import Works from "../components/works" // to be added later
+import Works from "../components/works"
 // import Preloader from '../components/preloader';
 
 const IndexPage = () => {
@@ -20,7 +20,7 @@ const IndexPage = () => {
         <Main/>
         <About />
         <Services />
-        {/* <Works/> */}
+        <Works/>
         <Contact/>
       </Layout>
     </div>
