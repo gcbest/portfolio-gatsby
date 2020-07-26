1688d3e3d4084fd8422f948f0822443263dce628
Godfrey Best <godfrey.c.best@gmail.com>
2020-07-25 23:23:40 -0400

    start card module

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-function Card({id}) {
+function Card({ image, alt, description, tags }) {
+    // return (
+    //     <figure className="card">
+    //         <img src={image} alt={alt} />
+    //         <figcaption>{description}</figcaption>
+    //         <figcaption>{tags.map(t => t.name)}</figcaption>
+    //     </figure>
+    // )
     return (
-        <div className="card">
-            Card {id}
-        </div>
+        <figure className="card">
+            <img src={image} alt={alt} />
+            <figcaption>{description}</figcaption>
+            <figcaption>{tags.map(t => t.name)}</figcaption>
+        </figure>
     )
 }
 
